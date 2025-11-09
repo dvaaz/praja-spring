2f7d67937bd4b8c169420914751c27051d94edba
dvaaz <darley.azevedo@gmail.com>
2025-11-09 17:21:19 -0300

    Refazendo a "parte 2" do projeto. Sem o SecurityConfiguration.java para agilidade de testes

--- src/main/java/projt4/praja/service/UsuarioService.java
+++ src/main/java/projt4/praja/service/UsuarioService.java
@@ -1,53 +1,53 @@
-package projt4.praja.service;
-
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.stereotype.Service;
-import projt4.praja.security.config.SecurityConfiguration;
-import projt4.praja.entity.Usuario;
-import projt4.praja.entity.dto.request.usuario.UsuarioDTORequest;
-import projt4.praja.entity.dto.response.usuario.UsuarioDTOResponse;
-import projt4.praja.repository.UsuarioRepository;
-import projt4.praja.security.authentication.JwtTokenService;
-
-@Service
-public class UsuarioService {
-    private AuthenticationManager authenticationManager;
-    private JwtTokenService jwtTokenService;
-    private SecurityConfiguration securityConfiguration;
-    private final UsuarioRepository usuarioRepository;
-		private RoleService roleService;
-
-		@Autowired
-		public UsuarioService(AuthenticationManager authenticationManager,
-		                      JwtTokenService jwtTokenService,SecurityConfiguration securityConfiguration,
-		                      UsuarioRepository usuarioRepository, RoleService roleService) {
-				this.authenticationManager = authenticationManager;
-				this.jwtTokenService = jwtTokenService;
-				this.securityConfiguration = securityConfiguration;
-				this.usuarioRepository = usuarioRepository;
-				this.roleService = roleService;
-		}
-
-		public UsuarioDTOResponse criar(UsuarioDTORequest dtoRequest){
-				// Sem Exception para roles inexistentes
-        Usuario usuario = new Usuario();
-        usuario.setNome(dtoRequest.nome());
-        usuario.setTelefone(dtoRequest.telefone());
-        usuario.setSenha(securityConfiguration.passwordEncoder().encode(dtoRequest.senha()));
-        usuario.setStatus(1);
-        usuario.setRoles((roleService.assertRole(dtoRequest.roleList())));
-        Usuario usuariosave = usuarioRepository.save(usuario);
-
-		    UsuarioDTOResponse usuarioDTO = new UsuarioDTOResponse();
-        usuarioDTO.setId(usuariosave.getId());
-        usuarioDTO.setNome(usuariosave.getNome());
-        usuarioDTO.setId(usuariosave.getId());
-        usuarioDTO.setTelefone(usuariosave.getTelefone());
-		    usuarioDTO.getRoleList(usuariosave.getRoles()); // verificar o uso de stream
-        return  usuarioDTO;
-    }
-
-}
+//package projt4.praja.service;
+//
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.stereotype.Service;
+//import projt4.praja.security.config.SecurityConfiguration;
+//import projt4.praja.entity.Usuario;
+//import projt4.praja.entity.dto.request.usuario.UsuarioDTORequest;
+//import projt4.praja.entity.dto.response.usuario.UsuarioDTOResponse;
+//import projt4.praja.repository.UsuarioRepository;
+//import projt4.praja.security.authentication.JwtTokenService;
+//
+//@Service
+//public class UsuarioService {
+//    private AuthenticationManager authenticationManager;
+//    private JwtTokenService jwtTokenService;
+//    private SecurityConfiguration securityConfiguration;
+//    private final UsuarioRepository usuarioRepository;
+//		private RoleService roleService;
+//
+//		@Autowired
+//		public UsuarioService(AuthenticationManager authenticationManager,
+//		                      JwtTokenService jwtTokenService,SecurityConfiguration securityConfiguration,
+//		                      UsuarioRepository usuarioRepository, RoleService roleService) {
+//				this.authenticationManager = authenticationManager;
+//				this.jwtTokenService = jwtTokenService;
+//				this.securityConfiguration = securityConfiguration;
+//				this.usuarioRepository = usuarioRepository;
+//				this.roleService = roleService;
+//		}
+//
+//		public UsuarioDTOResponse criar(UsuarioDTORequest dtoRequest){
+//				// Sem Exception para roles inexistentes
+//        Usuario usuario = new Usuario();
+//        usuario.setNome(dtoRequest.nome());
+//        usuario.setTelefone(dtoRequest.telefone());
+//        usuario.setSenha(securityConfiguration.passwordEncoder().encode(dtoRequest.senha()));
+//        usuario.setStatus(1);
+//        usuario.setRoles((roleService.assertRole(dtoRequest.roleList())));
+//        Usuario usuariosave = usuarioRepository.save(usuario);
+//
+//		    UsuarioDTOResponse usuarioDTO = new UsuarioDTOResponse();
+//        usuarioDTO.setId(usuariosave.getId());
+//        usuarioDTO.setNome(usuariosave.getNome());
+//        usuarioDTO.setId(usuariosave.getId());
+//        usuarioDTO.setTelefone(usuariosave.getTelefone());
+//		    usuarioDTO.getRoleList(usuariosave.getRoles()); // verificar o uso de stream
+//        return  usuarioDTO;
+//    }
+//
+//}
