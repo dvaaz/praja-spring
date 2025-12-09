86d9a70e96865d5c9ae54801412904e2c1cc70a6
dvaaz <darley.azevedo@gmail.com>
2025-12-09 11:40:01 -0300

    inicio da pagina index.html

---
+++ src/main/java/projt4/praja/controller/WebController.java
@@ -0,0 +1,15 @@
+package projt4.praja.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@CrossOrigin("*")
+public class WebController {
+  @GetMapping("/")
+  public String home(){
+    return "forward:/index.html";
+  }
+}
