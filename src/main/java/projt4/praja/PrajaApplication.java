cdc5edddad61d40bdc5be28c3dac0fcd609343fa
dvaaz <darley.azevedo@gmail.com>
2025-12-11 05:31:53 -0300

    index com qr

--- src/main/java/projt4/praja/PrajaApplication.java
+++ src/main/java/projt4/praja/PrajaApplication.java
@@ -1,11 +1,8 @@
 package projt4.praja;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class PrajaApplication {
